9fad7f36e4f2969b4decffe30a7cf74919a5f959
soyidwahyud <soyidwahyu4@gmail.com>
2021-03-29 11:50:11 +0700

    Hitung Vikor Updated

--- Src/Frontend - admin/src/views/Base/Tabel/HitungVikorTables/HitungVikorTables.js
+++ Src/Frontend - admin/src/views/Base/Tabel/HitungVikorTables/HitungVikorTables.js
@@ -8,7 +8,7 @@ import {
   Table
 } from 'reactstrap';
 
-import {getMatriks, getNilaiMax, getNilaiMin} from '../../../../Function/VikorFunction'
+import {getMatriks, getNilaiMax, getNilaiMin, getTabelRanking} from '../../../../Function/VikorFunction'
 
 
 class HitungVikorTables extends Component {
@@ -20,15 +20,19 @@ class HitungVikorTables extends Component {
       kebahasaan:'',
       penyajian:'',
       kegrafikaan:'',
+      ranking:'',
+      nama_buku:'',
       matriks_keputusan:[],
       nilai_maks:[],
-      nilai_min:[]
+      nilai_min:[],
+      tabel_ranking:[]
     }
   }
   componentDidMount() {
     this.getMatriks()
     this.getNilaiMaks()
     this.getNilaiMin()
+    this.getTabelRanking()
   }
 
   //SELECT Matriks
@@ -91,105 +95,151 @@ class HitungVikorTables extends Component {
     })
   }
 
+  //SELECT Tabel Ranking
+  getTabelRanking = () => {
+    getTabelRanking().then(tabel_ranking => {
+      this.setState({
+          ranking: '',
+          kode_buku: '',
+          nama_buku: '',
+          tabel_ranking: [...tabel_ranking]
+        },
+        () => {
+          console.log(
+            this.state.ranking,
+            this.state.kode_buku,
+            this.state.nama_buku)
+        })
+    })
+  }
+
   render() {
     return (
       <div className="animated fadeIn">
-        <Row>
-          {/*Matriks Keputusan*/}
-          <Col xs="12" lg="10">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i> Tabel Matriks Keputusan
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead>
-                  <tr>
-                    <th>Alternatif</th>
-                    <th>C1</th>
-                    <th>C2</th>
-                    <th>C3</th>
-                    <th>C4</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {this.state.matriks_keputusan.map((matriks_keputusan, index) => (
-                    <tr key={index}>
-                      <td>{matriks_keputusan[0]}</td>
-                      <td>{matriks_keputusan[1]}</td>
-                      <td>{matriks_keputusan[2]}</td>
-                      <td>{matriks_keputusan[3]}</td>
-                      <td>{matriks_keputusan[4]}</td>
-                    </tr>
-                  ))}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-        {/*  Nilai Maks */}
-          <Col xs="12" lg="10">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i> Tabel Nilai Maksimum
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead>
-                  <tr>
-                    <th>Kelayakan Isi</th>
-                    <th>Kebahasaan</th>
-                    <th>Penyajian</th>
-                    <th>Kegrafikaan</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {this.state.nilai_maks.map((nilai_maks, index) => (
-                    <tr key={index}>
-                      <td>{nilai_maks[0]}</td>
-                      <td>{nilai_maks[1]}</td>
-                      <td>{nilai_maks[2]}</td>
-                      <td>{nilai_maks[3]}</td>
-                    </tr>
-                  ))}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-        {/*  Nilai Min */}
-          <Col xs="12" lg="10">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i> Tabel Nilai Minimum
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead>
-                  <tr>
-                    <th>Kelayakan Isi</th>
-                    <th>Kebahasaan</th>
-                    <th>Penyajian</th>
-                    <th>Kegrafikaan</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {this.state.nilai_min.map((nilai_min, index) => (
-                    <tr key={index}>
-                      <td>{nilai_min[0]}</td>
-                      <td>{nilai_min[1]}</td>
-                      <td>{nilai_min[2]}</td>
-                      <td>{nilai_min[3]}</td>
-                    </tr>
-                  ))}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
+      <Row>
+      {/*Matriks Keputusan*/}
+      <Col xs="12" lg="10">
+      <Card>
+      <CardHeader>
+      <i className="fa fa-align-justify"></i> Tabel Matriks Keputusan
+      </CardHeader>
+      <CardBody>
+      <Table responsive>
+    <thead>
+    <tr>
+    <th>Alternatif</th>
+    <th>C1</th>
+    <th>C2</th>
+    <th>C3</th>
+    <th>C4</th>
+    </tr>
+    </thead>
+    <tbody>
+    {this.state.matriks_keputusan.map((matriks_keputusan, index) => (
+        <tr key={index}>
+      <td>{matriks_keputusan[0]}</td>
+      <td>{matriks_keputusan[1]}</td>
+      <td>{matriks_keputusan[2]}</td>
+      <td>{matriks_keputusan[3]}</td>
+      <td>{matriks_keputusan[4]}</td>
+      </tr>
+  ))}
+  </tbody>
+    </Table>
+    </CardBody>
+    </Card>
+    </Col>
+    {/*  Nilai Maks */}
+  <Col xs="12" lg="10">
+      <Card>
+      <CardHeader>
+      <i className="fa fa-align-justify"></i> Tabel Nilai Maksimum
+      </CardHeader>
+      <CardBody>
+      <Table responsive>
+    <thead>
+    <tr>
+    <th>Kelayakan Isi</th>
+    <th>Kebahasaan</th>
+    <th>Penyajian</th>
+    <th>Kegrafikaan</th>
+    </tr>
+    </thead>
+    <tbody>
+    {this.state.nilai_maks.map((nilai_maks, index) => (
+        <tr key={index}>
+      <td>{nilai_maks[0]}</td>
+      <td>{nilai_maks[1]}</td>
+      <td>{nilai_maks[2]}</td>
+      <td>{nilai_maks[3]}</td>
+      </tr>
+  ))}
+  </tbody>
+    </Table>
+    </CardBody>
+    </Card>
+    </Col>
+    {/*  Nilai Min */}
+  <Col xs="12" lg="10">
+      <Card>
+      <CardHeader>
+      <i className="fa fa-align-justify"></i> Tabel Nilai Minimum
+      </CardHeader>
+      <CardBody>
+      <Table responsive>
+    <thead>
+    <tr>
+    <th>Kelayakan Isi</th>
+    <th>Kebahasaan</th>
+    <th>Penyajian</th>
+    <th>Kegrafikaan</th>
+    </tr>
+    </thead>
+    <tbody>
+    {this.state.nilai_min.map((nilai_min, index) => (
+        <tr key={index}>
+      <td>{nilai_min[0]}</td>
+      <td>{nilai_min[1]}</td>
+      <td>{nilai_min[2]}</td>
+      <td>{nilai_min[3]}</td>
+      </tr>
+  ))}
+  </tbody>
+    </Table>
+    </CardBody>
+    </Card>
+    </Col>
+    {/*  Tabel Ranking */}
+  <Col xs="12" lg="10">
+      <Card>
+      <CardHeader>
+      <i className="fa fa-align-justify"></i> Tabel Ranking
+      </CardHeader>
+      <CardBody>
+      <Table responsive>
+    <thead>
+    <tr>
+    <th>Ranking</th>
+    <th>Kode Buku</th>
+    <th>Nama Buku</th>
+    </tr>
+    </thead>
+    <tbody>
+    {this.state.tabel_ranking.map((tabel_ranking, index) => (
+        <tr key={index}>
+      <td>{tabel_ranking[0]}</td>
+      <td>{tabel_ranking[1]}</td>
+      <td>{tabel_ranking[2]}</td>
+      </tr>
+  ))}
+  </tbody>
+    </Table>
+    </CardBody>
+    </Card>
+    </Col>
+    </Row>
+    </div>
+  );
   }
 }
 
